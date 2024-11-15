6fe5d8fdf7925102cf1fbc468d164d1b8330e55b
Modante <34290616+modante@users.noreply.github.com>
2024-11-15 11:14:50 +0100

    Delete RaspiLoopStation.sh

--- RaspiLoopStation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-killall fluidsynth
-jack_control stop
-killall jackd
-cd ~/shared/RaspiLoopStation/
-until python ./RaspiLoopStation.py
-do
-read -t 2 -p "I am going to wait for 2 seconds only ..."
-done
