41e2ed15640fd56e9c24294d741b02e5fb7181e5
Modante <34290616+modante@users.noreply.github.com>
2024-11-17 12:41:22 +0100

    Update LooPyStation.sh

--- LooPyStation.sh
+++ LooPyStation.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 killall fluidsynth
-#jack_control stop
-#killall jackd
 cd ~/shared/LooPyStation/
-until python ./LooPyStation.py
-do
-#jack_control stop
-#killall jackd
-read -t 1 -p "I am going to wait for 1 second ..."
-done
+if pgrep -x "python" > /dev/null; then
+    echo "Python is running."
+else
+    echo "Python is NOT running."
+    until python ./LooPyStation.py
+        do
+        read -t 1 -p "I am going to wait for 1 second ..."
+    done
+fi
+
+
+
